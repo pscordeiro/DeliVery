aa67c7040e86c19a47fd2378ac87ddd1aa295923
Paulo Cordeiro <ps.cordeiro01@gmail.com>
2021-06-22 23:17:42 -0300

    CHANGE: Add algumas validações de input, finalizado DAO de cadProduto

--- ProjetoIntegrador/src/main/java/br/edu/opet/entity/model/Produto.java
+++ ProjetoIntegrador/src/main/java/br/edu/opet/entity/model/Produto.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import br.edu.opet.model.entity.dao.ProdutoDAO;
 
@@ -12,10 +15,17 @@ import br.edu.opet.model.entity.dao.ProdutoDAO;
 public class Produto extends ProdutoDAO {
 	
 	private int Idf_Produto;
+	
+	@Size(max=100)
 	private String Desc_Produto;
+    
+	@Pattern(regexp = "(\\$)?[0-9]+\\.*[0-9]*", message = "Digite apenas números com ponto. Ex: R$ 10.50")
 	private double Valor_Produto;
-	private int quantidade;
-	private String Url_Produto;
+    
+    @NotNull(message = "Por favor digite um valor")
+    private int quantidade;
+	
+    private String Url_Produto;
 	
 	public int getIdf_Produto() {
 		return Idf_Produto;
@@ -51,9 +61,8 @@ public class Produto extends ProdutoDAO {
 		return super.listarProdutos();
 	}
 	
-	public boolean cadastrarProduto() {
-		//return super.cadastrarProduto();
-		return true;
+	public boolean cadastrarProduto(Produto prod) {
+		return super.cadastrarProduto(prod);
 	}
 
 }
